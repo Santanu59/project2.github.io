beefe3cd0ce4a4468f5f40627af191684acefc32
Santanu59 <103487133+Santanu59@users.noreply.github.com>
2022-04-16 09:00:49 -0700

    initial

---
+++ script.js
@@ -0,0 +1,16 @@
+var manubtn = document.getElementById("manubtn")
+var sidenav = document.getElementById("sidenav")
+var manu = document.getElementById("manu")
+
+sidenav.style.right = "-250px";
+
+manubtn.onclick = function(){
+    if(sidenav.style.right == "-250px"){
+        sidenav.style.right = "0";
+        // manu.src = "image/close.png"
+    }
+    else{
+        sidenav.style.right = "-250px";
+        // manu.src = "image/manu.png";
+    }
+}
